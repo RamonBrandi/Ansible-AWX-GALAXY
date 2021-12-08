064940f09a1c3fc4d6de0473aab22978384319b8
ramon brandi <ramonbrand@gmail.com>
2021-12-08 18:29:47 -0300

    update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,9 +1,9 @@
 #!/bin/sh 
 sudo dpkg --configure -a
-sudo apt update
+sudo apt update -y
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible -y
 mkdir projeto
 cd projeto
 git clone https://github.com/RamonBrandi/Ansible-AWX-GALAXY.git
